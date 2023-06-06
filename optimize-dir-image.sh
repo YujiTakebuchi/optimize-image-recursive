b9d0c07e35b84320c288f035a906a8fd21d7e0fd
YujiTakebuchi <takeyou.swift@gmail.com>
2023-06-06 19:20:12 +0900

    指定したディレクトリ配下の全てのディレクトリに含まれる全てのJPGファイルを処理するよう修正

--- optimize-dir-image.sh
+++ optimize-dir-image.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 [ "x$1" = "x" ] && exit 1
 #引数にファイルパスを代入
-DIR=$1
+SRC=$1
 
-for file in `find $DIR -name '*.JPG'`; do
-  echo `basename $file .JPG`
+cd $SRC
+
+for dir in `find . -type d -depth 1 -maxdepth 1`; do
+    dirName=`basename $dir`
+    echo $dir
+    echo $dirName
+    DIST="opt-$dirName"
+    mkdir $DIST
+    echo $DIST
+    for file in `find $dir -name '*.JPG'`; do
+        fileName=`basename $file .JPG`
+        echo $fileName
+        ffmpeg -i $file -vf "scale=1280:-1" -q 2 "${DIST}/${fileName}.JPG" -loglevel error
+    done
 done
+
+cd ..
